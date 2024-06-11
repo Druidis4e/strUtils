a36d6159c85293f2a5e452a1d9c46a525f01ec79
Druidis4e <83408003+Druidis4e@users.noreply.github.com>
2024-06-11 11:07:26 +0300

    Update StrUtils.go

--- StrUtils.go
+++ StrUtils.go
@@ -2,8 +2,13 @@ package StrUtils
 
 import (
 	myLib "github.com/IamDruid84/mainRepo"
+	"fmt"
 )
 
 func StrUtils() {
 	myLib.DoSomething(0,1)
+}
+
+func StrUtilsSelf() {
+	fmt.Println("strutilsSelf")
 }
